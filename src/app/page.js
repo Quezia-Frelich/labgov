4536ac92ef91e6bf7c1c166df77b9380f616881e
Your Name <joao.gomes@labofcodes.com>
2024-11-01 15:09:11 +0000

    Comentar programação

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,23 @@
-"use client"
+"use client";
 
-import Section_welcome from './ui/sections/home/section-welcome';
-import Section_numbers from './ui/sections/home/section-numbers';
-import Section_location from './ui/sections/global/section-location';
-import Section_cta from './ui/sections/global/section-cta';
-import Section_sponsors from './ui/sections/global/section-sponsors';
-import Section_programs from './ui/sections/home/section-programs';
-import Section_speakers from './ui/sections/home/section-speakers';
+import Section_welcome from "./ui/sections/home/section-welcome";
+import Section_numbers from "./ui/sections/home/section-numbers";
+import Section_location from "./ui/sections/global/section-location";
+import Section_cta from "./ui/sections/global/section-cta";
+import Section_sponsors from "./ui/sections/global/section-sponsors";
+import Section_programs from "./ui/sections/home/section-programs";
+import Section_speakers from "./ui/sections/home/section-speakers";
 
 export default function Home() {
-
   return (
     <>
-    <Section_welcome />
-    <Section_numbers />
-    <Section_speakers />
-    <Section_location showMap={ true }/>
-    <Section_cta />
-    <Section_sponsors />
-    <Section_programs />
+      <Section_welcome />
+      <Section_numbers />
+      <Section_speakers />
+      <Section_location showMap={true} />
+      {/* <Section_cta /> */}
+      <Section_sponsors />
+      <Section_programs />
     </>
   );
 }
